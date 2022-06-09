6b58ff8003c1724fe21af1511901c491c412c447
Uusuff <uusuuff@gmail.com>
2022-06-09 15:12:32 +0300

    When-you-click-on-the-user's-name-to-display-the-posts-that-he-wrote

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
 
 function App() {
    const [users, setUsers] = useState([]);
-   
+   const [posts, setPosts] = useState([]);
 
    async function getUsers() {
       const data = await fetch('https://gorest.co.in/public/v2/users');
@@ -13,22 +13,39 @@ function App() {
       setUsers(users);
    }
 
-   useEffect(() => {
+   async function getPosts(user_id) {
+      const data = await fetch('https://gorest.co.in/public/v2/users/' + user_id + '/posts');
+      const posts = await data.json();
+     
+      setPosts(posts);
+   }
+
+
+   useEffect(() =>{
       getUsers();
    }, []);
 
   return (
-      <div className='div'>   
+      <div className='content'>   
          <ul className='list'>
             {users.map((user) => {
               return(
-                  <li className='user' key={user.id} >
+                  <li className='user' key={user.id} onClick={() => getPosts(user.id)} >
                      {user.name}
                   </li>
                );
             })}   
-         </ul>  
+         </ul>
+         <div className='post'>
+            {posts.map((post) => {
+               return(
+                  <span  key={post.id}>Post: {post.title}</span> 
+               )
+            })
+         }
+         </div>
       </div>
+
   );
 }
 
